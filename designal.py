82da620c5b2426b49a242f3175439f1a5fa3fd81
Debanjum Singh Solanky <debanjum@gmail.com>
2021-09-26 17:58:27 -0700

    Store to org in Conversation Tree Format
    - Extract Conversation Name from db
    - Commit includes updated Signal.org output

--- designal.py
+++ designal.py
@@ -11,27 +11,45 @@ class Conversation:
     """
     A Signal Conversation
     """
-    def __init__(self, thread_id) -> None:
+    def __init__(self, thread_id, name) -> None:
         self.thread_id = thread_id
         self.messages = []
+        self.name = name
+        self.current_users = []
+
+    def export(self, indent=0):
+        ret = ""
+        ret += f"{'*'*indent} {self.name}\n"
+        for message in sorted(self.messages, key=lambda x: x.date):
+            ret += f"{'*'*(indent+1)} {message.heading_str()}\n"
+            ret += f'{message.body}\n'
+        return ret
+
+    def add_message(self, message: "Message"):
+        self.messages.append(message)
 
 
 class User:
     """
     A Signal User
     """
-    pass
+    def __init__(self) -> None:
+        pass
 
 
 class Message:
     """
     A Signal Message
     """
-    def __init__(self, date, body, thread) -> None:
+    def __init__(self, date, body, conversation: Conversation) -> None:
         self.date = date
-        self.body = body
-        self.thread = thread
+        self.body = body if body else ""
+        self.conversation = conversation
         self.sender = None
+
+    def heading_str(self) -> str:
+        body_header = self.body.split('\n')[0]
+        return f'[{self.date.strftime("%Y-%m-%d %H:%M")}] {self.sender}: {body_header}'
 
     def __repr__(self) -> str:
         return f'[{self.date.strftime("%Y-%m-%d %H:%M")}]: {self.sender}: {self.body}'
@@ -70,6 +88,18 @@ class Signal:
         mms = cur.execute('select date, body, thread_id from mms;').fetchall()
         messages = sms + mms
 
+        # Create Thread Id to Conversation Name Map
+        conversation_name_dict = {}
+        threads = cur.execute('select _id, thread_recipient_id from thread;').fetchall()
+        for thread in threads:
+            recipients = cur.execute(f'select system_display_name, group_id from recipient where _id={thread[1]};').fetchall()
+            for recipient in recipients:
+                if recipient[1]: #group id
+                    group = cur.execute(f'select title from groups where recipient_id={thread[1]};').fetchone()
+                    conversation_name_dict[thread[0]] = group[0]
+                else:
+                    conversation_name_dict[thread[0]] = recipient[0]
+
         # Load Signal data into VDB in an intermediate representation
         for message in messages:
             dt = datetime.utcfromtimestamp(message[0]/1e3)
@@ -77,16 +107,16 @@ class Signal:
 
             conversation = [t for t in vdb.conversations if t.thread_id == thread_id]
             if not conversation:
-                conversation = Conversation(thread_id)
+                conversation = Conversation(thread_id, name=conversation_name_dict[thread_id])
                 vdb.conversations.append(conversation)
             else:
                 conversation = conversation[0]
 
-            vdb.messages.append(
+            conversation.add_message(
                 Message(
                     date=dt,
                     body=message[1],
-                    thread=conversation
+                    conversation=conversation
                     )
                 )
         conn.close()
@@ -97,10 +127,8 @@ class Signal:
         """Create Org File from Virtual DB
         """
         with open(outfile, 'w') as fout:
-            for message in self.vdb.messages:
-                if self.verbose > 0:
-                    print(f'writing message {message}')
-                fout.write(f'* {message} \n')
+            for conversation in self.vdb.conversations:
+                fout.write(conversation.export(indent=1))
 
 
 if __name__ == '__main__':
