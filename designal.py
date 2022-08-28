86ed7387370ce9d5be73a93f3f1d662e3cf8020f
Sybil <sybil@haletic.com>
2022-08-28 22:58:58 +0000

    Link to message being replied to from child message

--- designal.py
+++ designal.py
@@ -12,6 +12,7 @@ class Conversation:
     """
     A Signal Conversation
     """
+
     def __init__(self, thread_id, name) -> None:
         self.thread_id = thread_id
         self.messages = []
@@ -21,9 +22,12 @@ class Conversation:
     def export(self, indent=0):
         ret = ""
         ret += f"{'*'*indent} {self.name}\n"
-        for message in sorted(self.messages, key=lambda x: x.date):
+        for message in sorted(self.messages, key=lambda x: x.date_received):
             ret += f"{'*'*(indent+1)} {message.heading_str()}\n"
-            ret += f'{message.body}\n'
+            ret += f":PROPERTIES:\n:CUSTOM_ID: {message.date_sent.isoformat()}\n:END:\n"
+            ret += f"{message.body}\n"
+            if message.parent:
+                ret += f"[[#{message.parent.date_sent.isoformat()}][{message.parent.body_header}]]\n"
         return ret
 
     def add_message(self, message: "Message"):
@@ -37,8 +41,10 @@ class User:
     """
     A Signal User
     """
+
     def __init__(self, name) -> None:
         self.name = name
+
 
 # create enum class
 class MessageType(Enum):
@@ -46,32 +52,49 @@ class MessageType(Enum):
     OUTGOING = auto()
     OTHER = auto()
 
+
 class Message:
     """
     A Signal Message
     """
-    def __init__(self, date, body, sender, message_type: MessageType, conversation: Conversation) -> None:
-        self.date = date
+
+    def __init__(
+        self,
+        date_received,
+        date_sent,
+        body,
+        sender,
+        message_type: MessageType,
+        conversation: Conversation,
+        parent: "Message",
+    ) -> None:
+        self.date_received = date_received
+        self.date_sent = date_sent
         self.body = body if body else ""
         self.conversation = conversation
         self.message_type = message_type
         self.sender = sender
+        self.parent = parent
+
+    @property
+    def body_header(self):
+        return self.body.split("\n")[0]
 
     def heading_str(self) -> str:
-        body_header = self.body.split('\n')[0]
-        return f'[{self.date.strftime("%Y-%m-%d %H:%M")}] {self.sender}: {body_header}'
+        return f'[{self.date_received.strftime("%Y-%m-%d %H:%M")}] {self.sender}: {self.body_header}'
 
     def __repr__(self) -> str:
-        return f'[{self.date.strftime("%Y-%m-%d %H:%M")}]: {self.sender}: {self.body}'
+        return f'[{self.date_received.strftime("%Y-%m-%d %H:%M")}]: {self.sender}: {self.body}'
 
 
 class SignalVDB:
     """
     In-Memory DB. Stores an Intermediate Representation of Signal conversations
     """
+
     # contains group, user, message etc
     def __init__(self) -> None:
-        self.conversations = [] 
+        self.conversations = []
         self.users = []
         self.messages = []
 
@@ -91,27 +114,53 @@ class Signal:
         # open connection to sqlite db
         vdb = SignalVDB()
         conn = sqlite3.connect(sqlfile)
+        conn.row_factory = sqlite3.Row
         cur = conn.cursor()
 
         # Get all plain and rich-text Signal messages
-        sms = cur.execute('select date, body, thread_id, address, type from sms;').fetchall()
-        mms = cur.execute('select date, body, thread_id, address, msg_box from mms;').fetchall()
+        sms = cur.execute(
+            "select date_sent, date, body, thread_id, address, type from sms;"
+        ).fetchall()
+        mms = cur.execute(
+            "select date, date_received, body, thread_id, address, msg_box, quote_id from mms;"
+        ).fetchall()
+
+        sms = [dict(m) for m in sms]
+        mms = [dict(m) for m in mms]
+
+        for m in sms:
+            m["date_received"] = m.pop("date")
+            m["quote_id"] = 0
+
+        for m in mms:
+            m["date_sent"] = m.pop("date")
+            m['type'] = m.pop("msg_box")
+
         messages = sms + mms
 
+        # sort messages by date column in db
+        messages.sort(key=lambda m: m["date_received"])
+
         # Create List of Users
-        recipients = cur.execute(f'select _id, system_display_name, group_id from recipient').fetchall()
+        recipients = cur.execute(
+            f"select _id, system_display_name, group_id from recipient"
+        ).fetchall()
         user_dict = dict()
         for recipient in recipients:
             user_dict[recipient[0]] = User(recipient[1])
 
         # Create Thread Id to Conversation Name Map
         conversation_name_dict = {}
-        threads = cur.execute('select _id, thread_recipient_id from thread;').fetchall()
+        threads = cur.execute("select _id, thread_recipient_id from thread;").fetchall()
         for thread in threads:
-            recipients = cur.execute(f'select system_display_name, group_id from recipient where _id={thread[1]};').fetchall()
+            recipients = cur.execute(
+                f"select system_display_name, group_id from recipient where _id={thread[1]};"
+            ).fetchall()
             for recipient in recipients:
-                if recipient[1]: #group id
-                    group = cur.execute(f'select title from groups where recipient_id={thread[1]};').fetchone()
+                if recipient[1]:  # group id
+                    group = cur.execute(
+                        f"select title from groups where recipient_id={thread[1]};"
+                    ).fetchone()
                     conversation_name_dict[thread[0]] = group[0]
                 else:
                     conversation_name_dict[thread[0]] = recipient[0]
@@ -119,60 +168,88 @@ class Signal:
         # Load Signal data into VDB in an intermediate representation
         for message in messages:
             # Ignore Non Processable Messages
-            if message[1] == '' or message[1] is None:
+            if message["thread_id"] == "" or message["thread_id"] is None:
                 continue
-            dt = datetime.utcfromtimestamp(message[0]/1e3)
-            thread_id = message[2]
+            thread_id = message["thread_id"]
 
             conversation = [t for t in vdb.conversations if t.thread_id == thread_id]
             if not conversation:
-                conversation = Conversation(thread_id, name=conversation_name_dict[thread_id])
+                conversation = Conversation(
+                    thread_id, name=conversation_name_dict[thread_id]
+                )
                 vdb.conversations.append(conversation)
             else:
                 conversation = conversation[0]
 
             # Identify Message Sender
-            if message[4] == 10485783:
+            if message["type"] == 10485783:
                 message_type = MessageType.INCOMING
                 sender = "Me"
-            elif message[4] == 10485780:
+            elif message["type"] == 10485780:
                 message_type = MessageType.OUTGOING
-                sender = user_dict[message[3]].name
+                sender = user_dict[message["address"]].name
             else:
                 continue
 
+            parent_message = None
+            if message['quote_id']:
+                parent_message = [
+                    m
+                    for m in conversation.messages
+                    if m.date_sent == datetime.utcfromtimestamp(message["quote_id"] / 1e3)
+                ][0]
+
             conversation.add_message(
                 Message(
-                    date=dt,
-                    body=message[1],
-                    sender = sender,
-                    message_type= message_type,
-                    conversation=conversation
-                    )
+                    date_received=datetime.utcfromtimestamp(message['date_received'] / 1e3),
+                    date_sent=datetime.utcfromtimestamp(message["date_sent"] / 1e3),
+                    body=message['body'],
+                    sender=sender,
+                    message_type=message_type,
+                    conversation=conversation,
+                    parent=parent_message,
                 )
+            )
         conn.close()
 
         return vdb
 
     def export_org(self, outfile):
-        """Create Org File from Virtual DB
-        """
-        with open(outfile, 'w') as fout:
+        """Create Org File from Virtual DB"""
+        with open(outfile, "w") as fout:
             for conversation in self.vdb.conversations:
                 fout.write(conversation.export(indent=1))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Setup Argument Parser for the Commandline Interface
     parser = argparse.ArgumentParser(description="An Exporter of Signal Conversations")
-    parser.add_argument('--sql-file', '-i', type=pathlib.Path, required=True, help="Sqlite DB with decrypted Signal conversations")
-    parser.add_argument('--output-file', '-o', type=pathlib.Path, required=True, help="Export file for Signal conversations")
-    parser.add_argument('--verbose', '-v', action='count', default=0, help="Show verbose conversion logs. Default: 0")
+    parser.add_argument(
+        "--sql-file",
+        "-i",
+        type=pathlib.Path,
+        required=True,
+        help="Sqlite DB with decrypted Signal conversations",
+    )
+    parser.add_argument(
+        "--output-file",
+        "-o",
+        type=pathlib.Path,
+        required=True,
+        help="Export file for Signal conversations",
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=0,
+        help="Show verbose conversion logs. Default: 0",
+    )
     args = parser.parse_args()
 
     # Load Signal conversations into In-Memory from Sqlite DB
     signal = Signal(sqlfile=args.sql_file, verbose=args.verbose)
 
     # Export Signal conversations to Org-Mode
-    if args.output_file.suffix == '.org':
+    if args.output_file.suffix == ".org":
         signal.export_org(args.output_file)
